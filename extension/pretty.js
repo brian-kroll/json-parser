d2bb8de744a9710b19a15c23873791ff6a7714c3
Brian Kroll <b.kroll@searchmetrics.com>
2017-06-16 12:41:49 +0200

    Formatting mainly

--- extension/pretty.js
+++ extension/pretty.js
@@ -1,8 +1,11 @@
+var frameTemplate;
+frameTemplate = '<html><head></head><body class="json-parse">{{{ contents }}}</body></html>';
+
 var errorTemplate;
 errorTemplate = '<h1>Crap, you got an error.</h1>';
-errorTemplate += '<p>Here\'s what the server says: <div class="message">{{ error_message }}</div></p>';
-errorTemplate += '<p>Here\'s where the error occurred: <div class="message">{{ exception.file }}</div>';
-errorTemplate += '<p>No Help? Here the raw error: {{ raw }}</p>';
+errorTemplate += '<h2>Here\'s what the server says: </h2><div class="message">{{{ error_message }}}</div>';
+errorTemplate += '<h2>Here\'s where the error occurred:</h2> <div class="message">{{ exception.file }}:{{ exception.line }}</div>';
+errorTemplate += '<h2>No Help? Here the raw error: </h2> {{{ raw }}}';
 
 var speedometer = '<canvas id="tutorial" width="440" height="220">';
 speedometer += 'Canvas not available.';
@@ -19,11 +22,21 @@ catch (exception) {
 
 
 function buildDocument(message) {
+    var frame = Handlebars.compile(frameTemplate);
     if (message.hasOwnProperty('error')) {
         var template = Handlebars.compile(errorTemplate);
 
-        document.body.className = 'error';
+        document.body.className = 'json-parse error';
         message.raw = document.body.innerHTML;
-        document.body.innerHTML = template(message);
+
+        message.error_message = nl2br(message.error_message);
+        var contents = template(message);
+
+        document.body.innerHTML = frame({"contents": contents});
     }
+}
+
+function nl2br (str, is_xhtml) {
+    var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
+    return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
 }
