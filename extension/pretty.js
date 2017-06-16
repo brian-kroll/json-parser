ebe676972a98bc049cfc119e9612521c854c8c5b
Brian Kroll <b.kroll@searchmetrics.com>
2017-06-16 12:05:19 +0200

    Packable

--- extension/pretty.js
+++ extension/pretty.js
@@ -1,5 +1,13 @@
+var errorTemplate;
+errorTemplate = '<h1>Crap, you got an error.</h1>';
+errorTemplate += '<p>Here\'s what the server says: <div class="message">{{ error_message }}</div></p>';
+errorTemplate += '<p>Here\'s where the error occurred: <div class="message">{{ exception.file }}</div>';
+errorTemplate += '<p>No Help? Here the raw error: {{ raw }}</p>';
 
-var errorTemplate = '<h1>Crap, you got an error.</h1><p>Here\'s what the server says: <div class="message">{{ message }}</div></p>';
+var speedometer = '<canvas id="tutorial" width="440" height="220">';
+speedometer += 'Canvas not available.';
+speedometer += '</canvas>';
+
 try {
     messageObject = JSON.parse(document.body.textContent);
 
@@ -8,12 +16,14 @@ try {
 catch (exception) {
     console.log(exception);
 }
-function buildDocument(messageObject) {
-    if (messageObject.hasOwnProperty('error')) {
-        var template = Handlebars.compile(errorTemplate);
-        var ret = template({"message": messageObject.error_message});
-        document.body.className = 'error';
 
-        document.body.innerHTML = ret;
+
+function buildDocument(message) {
+    if (message.hasOwnProperty('error')) {
+        var template = Handlebars.compile(errorTemplate);
+
+        document.body.className = 'error';
+        message.raw = document.body.innerHTML;
+        document.body.innerHTML = template(message);
     }
 }
